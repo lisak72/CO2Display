83831f012ae30a0b998739270ca0d8c98ff148b4
liska <lisak72@seznam.cz>
2021-02-04 11:21:19 +0100

    display first functional v

---
+++ CO2Display/CO2Display.ino
@@ -0,0 +1,23 @@
+#include <TM1637Display.h>
+
+uint8_t pinClk=A5;
+uint8_t pinDIO=A4;
+TM1637Display display(pinClk, pinDIO);
+
+void setup() {
+  // put your setup code here, to run once:
+
+display.setSegments(4, 4, 0);
+display.setBrightness(7, true); //brightness 0-7, on
+
+}
+
+int num=0;
+void loop() {
+  // put your main code here, to run repeatedly:
+display.clear();
+display.showNumberDec(num); 
+num++;
+delay(1000);
+
+}
