e0b4f3845abcb79a5e621720b5d41f39758fd5c6
liska <lisak72@seznam.cz>
2021-02-04 12:45:22 +0100

    display v2

--- CO2Display/CO2Display.ino
+++ CO2Display/CO2Display.ino
@@ -9,15 +9,15 @@ void setup() {
 
 display.setSegments(4, 4, 0);
 display.setBrightness(7, true); //brightness 0-7, on
-
+display.clear();
 }
 
 int num=0;
 void loop() {
   // put your main code here, to run repeatedly:
-display.clear();
+//display.clear();
 display.showNumberDec(num); 
 num++;
-delay(1000);
+delay(100);
 
 }
